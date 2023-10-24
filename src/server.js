17e05891dc784b8f1c554c0d6985a1926ccd6c88
Hasan <hasanjonsadullayev97@gmail.com>
2023-10-24 21:17:09 +0500

    add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
 // Session setup
 const session = require('express-session');
 const pg = require('pg');
